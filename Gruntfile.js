c7b0cf666b5f8cc4977bf6d52f80ae452b2f61a3
Hideyuki <erichideyuki@gmail.com>
2017-05-04 13:53:00 -0300

    First commit;

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/// <binding />
+/**
+ * Created by eric.kono on 19/10/2016.
+ */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        sass: {
+            compressed: {
+                options: {
+                    style: "compressed"
+                },
+                files: [
+                    {
+                        cwd: "sass",
+                        src: ["**/*.sass", "**/*.scss"],
+                        dest: "dist/css",
+                        expand: true,
+                        ext: ".min.css"
+                    }
+                ]
+            },
+            normal: {
+                files: [
+                    {
+                        cwd: "sass",
+                        src: ["**/*.sass", "**/*.scss"],
+                        dest: "dist/css",
+                        expand: true,
+                        ext: ".css"
+                    }
+                ]
+            }
+        },
+        watch: {
+            sass: {
+                files: ["sass/**/*.sass", "sass/**/*.scss"],
+                tasks: ["sass"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-sass");
+
+    grunt.registerTask("increment-buildnumber", ["buildnumber", "update-build-config"]);
+    grunt.registerTask("default", ["watch"]);
+};
