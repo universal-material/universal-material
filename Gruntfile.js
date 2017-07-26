bf14ff61ca5f6d9988e3047e5f7b13a08a557904
Eric Kono <erichideyuki@gmail.com>
2017-07-26 08:43:55 -0300

    Adding bootstrap 4 utilities;

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,5 +78,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks('grunt-autoprefixer');
 
-    grunt.registerTask("default", ["watch"]);
+    grunt.registerTask("default", ["sass", "pug", "watch"]);
 };
