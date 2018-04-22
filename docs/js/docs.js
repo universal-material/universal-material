45e63adbd2e066ba2e6ba9190851a8a772acb19b
Eric Kono <erichideyuki@gmail.com>
2018-04-21 21:43:47 -0300

    Uwp styles:  - Dialog  - Text fields
    Refactoring text-fields base style;

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -23,25 +23,6 @@
         }
     }
 
-    function dropdown() {
-        var dropdowns = document.querySelectorAll('.dropdown');
-
-        function toggleDropdown(e) {
-            e.stopPropagation();
-            this.parentElement.classList.toggle("show");
-        }
-
-        document.body.addEventListener("click", function () {
-            dropdowns.forEach(function (dropdown) {
-                dropdown.classList.remove("show");
-            });
-        });
-
-        dropdowns.forEach(function (dropdown) {
-            dropdown.querySelector('button').addEventListener("click", toggleDropdown);
-        });
-    }
-
     document
         .getElementById("menu-toggle")
         .addEventListener("click", toggleSidebar);
@@ -54,7 +35,6 @@
 
 
     document.getElementById("main-content").addEventListener("scroll", mainContentScroll);
-    setTimeout(dropdown, 0);
 })();
 
 hljs.initHighlightingOnLoad();
